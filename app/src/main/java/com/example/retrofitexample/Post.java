17b04f0c508747f57d56e7bc3b8f223309ca19a2
mahesh <maheshmanoharan7251@gmail.com>
2023-08-05 12:57:56 +0530

    initial get posts api added

---
+++ app/src/main/java/com/example/retrofitexample/Post.java
@@ -0,0 +1,25 @@
+package com.example.retrofitexample;
+
+public class Post
+{
+    private int userId;
+    private int id;
+    private String title;
+    private String text;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
